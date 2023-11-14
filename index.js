ccc5cb1f0b64c3e748964289539e8f2d3e2ad969
Dascal Cosmin <dascalcosmin52@gmail.com>
2023-11-14 18:26:11 +0100

    small fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -13,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.post("/update_order", (req, res) => {
   const { orderId, note, attributes } = req.body;
-  console.log(req.body)
+  // console.log(req.body)
   
   const noteAttributes = [];
   Object.keys(attributes).forEach(attr => {
@@ -23,19 +24,63 @@ app.post("/update_order", (req, res) => {
     })
   })
 
-  fetch(`https://8cf85f.myshopify.com/admin/api/2023-10/orders/${orderId}.json`, {
-    method: "PUT",
+  axios
+  .put(`https://8cf85f.myshopify.com/admin/api/2023-10/orders/${orderId}.json`, 
+    JSON.stringify({ order: { id: orderId, note: note, note_attributes: noteAttributes } }), 
+    {
     headers: {
       "Content-Type": "application/json",
       "X-Shopify-Access-Token": "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"
     },
-    body: JSON.stringify({ order: { id: orderId, note: note, note_attributes: noteAttributes } })
   }).then(function (response) {
+    res.send("updated!")
+  }).catch(function (err) {
+    res.send(err)
+  })
+
+
+  // fetch(`https://www.google-analytics.com/mp/collect?measurement_id=<<MY ID IS HERE>>&api_secret=<<MY API SECRET HERE>>`, {
+  //   method: "PUT",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "X-Shopify-Access-Token": "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"
+  //   },
+  //   body: JSON.stringify({ order: { id: orderId, note: note, note_attributes: noteAttributes } })
+  // }).then(function (response) {
+  //   res.send("updated!")
+  // }).catch(function (err) {
+  //   res.send("error")
+  // })
+
+})
+
+app.post("/update", (req, res) => {
+  const { payload } = req.body;
+  // console.log(req.body)
+  console.log(payload)
+
+  const mesurementID = "G-MQCKRF3NKV";
+  const apiSecretKey = "GSNNsjcgRO-F-pLdNOFdpQ";
+  
+  // const noteAttributes = [];
+  // Object.keys(attributes).forEach(attr => {
+  //   noteAttributes.push({
+  //     name: attr,
+  //     value: attributes[attr]
+  //   })
+  // })
+
+  axios
+  .post(
+    `https://www.google-analytics.com/mp/collect?measurement_id=${mesurementID}&api_secret=${apiSecretKey}`,
+    JSON.stringify(payload)
+  )
+  .then(function (response) {
+    console.log(response)
     res.send("updated!")
   }).catch(function (err) {
     res.send("error")
   })
-
 })
 
 app.listen(port, () => {
